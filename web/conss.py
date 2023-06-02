a8656b35dc3060c23ec22bb36a0d9786c77e9706
nlubkov <nlubkov@gmail.com>
2023-06-01 19:05:32 -0500

    Updated README file1

--- web/conss.py
+++ web/conss.py
@@ -6,10 +6,6 @@ class claves:
     def __init__(self):
         pass
     def devolver_con(self):
-        return ["cerocodigo", "AEx_1237458", "cerocodigoweb", "107.170.92.160", '3306']
+        return ["root", "123456789", "cerocodigoweb", "127.0.0.1"]
     def devolver_int(self):
-        return ["cerocodigo", "AEx_1237458", "mysql", "107.170.92.160", '3306']
-    # def devolver_con(self):
-    #     return ["root", "123456789", "cerocodigoweb", "127.0.0.1"]
-    # def devolver_int(self):
-    #     return ["root", "123456789", "mysql", "127.0.0.1"]
+        return ["root", "123456789", "mysql", "127.0.0.1"]
