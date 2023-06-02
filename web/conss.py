875e33ab3aa189d8c903b024495955be51c63b52
nlubkov <33459732+nlubkov@users.noreply.github.com>
2023-06-01 20:26:49 -0500

    Update conss.py

--- web/conss.py
+++ web/conss.py
@@ -6,6 +6,6 @@ class claves:
     def __init__(self):
         pass
     def devolver_con(self):
-        return ["root", "123456789", "cerocodigoweb", "127.0.0.1"]
+        return ["root", "123456789", "cerocodigoweb", "127.0.0.1", '3306']
     def devolver_int(self):
-        return ["root", "123456789", "mysql", "127.0.0.1"]
+        return ["root", "123456789", "mysql", "127.0.0.1", '3306']
